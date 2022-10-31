68091e5055c8a84a1adf18b0cbd4ae1a90c75a06
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-10-31 05:31:26 +0000

    Working p6 with mpg for each can added to the table and average mpg displayed under the table for the whole fleet.

--- .ccls-cache/@home@runner@Week10HWCSCI130DillonHendrix/p6@main6.cpp
+++ .ccls-cache/@home@runner@Week10HWCSCI130DillonHendrix/p6@main6.cpp
@@ -16,7 +16,11 @@ struct CarRec         // Global declaration
 int CN;
 int MD;
 int GU;
+double MPG;
 };
+
+/*************** Function Prototype *******************/
+double milesperGallon(CarRec car[numRecs], int numRecs);
 
 int main() {
   int i;
@@ -27,11 +31,23 @@ int main() {
   CarRec car[numRecs] = {{25, 1450, 62}, {36, 3240, 136}, {44, 1792, 76}, {52, 2360, 105}, {68, 2114, 67}};
 
   cout << endl;
+
+  milesperGallon(car, numRecs);
+  
   cout << setiosflags(ios::left); // left-justify the output
   for (i = 0; i < numRecs; i++)
     cout << setw(13)  << car[i].CN
-         << setw(15) << car[i].MD
-         << setw(15)  << car[i].GU << endl;
+         << setw(15)  << car[i].MD
+         << setw(15)  << car[i].GU 
+         << setw(15)  << car[i].MPG << endl;
 
   return 0;
 }
+
+/******************* Function **********************/
+double milesperGallon(CarRec generic[numRecs], int numRecs) {
+  for (int i = 0; i < numRecs; i++)
+  generic[i].MPG = generic[i].MD / generic[i].GU;
+
+  return 0;
+  }
--- p6/main6.cpp
+++ p6/main6.cpp
@@ -16,22 +16,53 @@ struct CarRec         // Global declaration
 int CN;
 int MD;
 int GU;
+double MPG;
 };
+
+/*************** Function Prototype *******************/
+double milesperGallon(CarRec car[numRecs], int numRecs);
+double aveMPGfleet(CarRec generic[numRecs], int numRecs);
 
 int main() {
   int i;
   
-  cout << "Car Number   Miles Driven   Gallons Used " << endl <<
-          "----------   ------------   ------------ ";
+  cout << "Car Number   Miles Driven   Gallons Used   MPG" << endl <<
+          "----------   ------------   ------------   ---";
   
   CarRec car[numRecs] = {{25, 1450, 62}, {36, 3240, 136}, {44, 1792, 76}, {52, 2360, 105}, {68, 2114, 67}};
 
   cout << endl;
+
+  milesperGallon(car, numRecs);
+  
   cout << setiosflags(ios::left); // left-justify the output
   for (i = 0; i < numRecs; i++)
     cout << setw(13)  << car[i].CN
-         << setw(15) << car[i].MD
-         << setw(15)  << car[i].GU << endl;
+         << setw(15)  << car[i].MD
+         << setw(15)  << car[i].GU 
+         << setw(15)  << car[i].MPG << endl;
+
+  aveMPGfleet(car, numRecs);
+
+  return 0;
+}
+
+/******************* Function **********************/
+double milesperGallon(CarRec generic[numRecs], int numRecs) {
+  for (int i = 0; i < numRecs; i++)
+  generic[i].MPG = generic[i].MD / generic[i].GU;
+
+  return 0;
+  }
+
+double aveMPGfleet(CarRec generic[numRecs], int numRecs) {
+  float sum = 0;
+  float averageMPG = 0;
+  for (int i = 0; i < numRecs; i++)
+  sum = generic[i].MPG + sum;
+  averageMPG = sum / numRecs;
+  //cout << endl << "The sum of mpg is " << sum << endl;
+  cout << endl << "The average miles per gallon for the fleet is " << averageMPG << endl;
 
   return 0;
 }
