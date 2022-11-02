4c9b938e82cd05d6388a1169a194b04558d64b41
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-11-02 01:30:41 +0000

    working p2

--- .ccls-cache/@home@runner@Week10HWCSCI130DillonHendrix/p3@main3.cpp
+++ .ccls-cache/@home@runner@Week10HWCSCI130DillonHendrix/p3@main3.cpp
@@ -1,33 +1,33 @@
- /***************************************************
-  Programmer: Dillon Hendrix
-  Discription: Kattis problem that takes in an array of numbers then rearranges them into left most largest number and then output the amount of numbers it takes the reach the leftmost largest number and the numbers that it takes reach the leftmost largest number. 
-  Date: 10/30/22
+/***************************************************
+ Programmer: Dillon Hendrix
+ Discription: Kattis problem that takes in an array of numbers then rearranges
+them into left most largest number and then output the amount of numbers it
+takes the reach the leftmost largest number and the numbers that it takes reach
+the leftmost largest number. Date: 10/30/22
 ***************************************************/
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 /************** Function Prototype ****************/
-void fillArray(int list[], int listSize); 
-int indexLargestElement(const int generic[], int listSize);
+void fillArray(int list[], int listSize);
+int indexSort(const int generic[], int listSize);
 
 int main() {
-  // initialize varriables 
+  // initialize varriables
   int arraysize = 0;
-  //int number = 0;
+  // int number = 0;
   cout << "Enter array size: "; // comment out for kattis
   cin >> arraysize;
-  cout << endl; 
+  cout << endl;
   // Create an array
   int GIS[arraysize];
-  //int finalMax[number];
-  
-  // cin an array size and fill the array 
+  // int finalMax[number];
+
+  // cin an array size and fill the array
   fillArray(GIS, arraysize);
-  indexLargestElement(GIS, arraysize);
-  // Create a function that finds smallest to largest input but only outputs left most largest value increments and reads the amount of indexes taken to get to the left most largest value. This info then needs to be output into a new array the size of the indexes taken and the inputs required to get to the largest value. 
-  
+  indexSort(GIS, arraysize);
 }
 
 /************** Functions *******************/
@@ -36,38 +36,32 @@ void fillArray(int list[], int listSize) {
 
   cout << "Enter " << listSize << " integers: "; // comment out for kattis
 
-  int index;
-
-  for (index = 0; index < listSize; index++) {
+  for (int index = 0; index < listSize; index++) {
     cin >> list[index];
   }
-
   return;
 }
 
-
-int indexLargestElement(const int generic[], int listSize) {
-  int maxIndex = 0; 
-  int number = 4;
+int indexSort(const int generic[], int listSize) {
+  int maxIndex = 0;
+  int number = 0;
   int finalMax[number];
   int column = 0;
-  //cout << maxIndex << endl;
+  int newArraySize;
+  // cout << maxIndex << endl;
   for (int index = 0; index < listSize; index++) {
-    
-    if (maxIndex < generic[index]){
-      number ++;
+
+    if (maxIndex < generic[index]) {
+      number++;
       maxIndex = generic[index];
-     // }
-    //else if (maxIndex > generic[index])
-      for (int i = 0; i < listSize; i++){
-      finalMax[i] = maxIndex;
-      //column ++;
-      cout << finalMax[i] << endl;
-      //cout << i << endl;
-      }
-    //cout << index << endl;
+      finalMax[number] = maxIndex;
+      // cout << finalMax[index] << endl;
+      cout << maxIndex << "  ";
+      newArraySize = number;
+      cout << "Array size is" << newArraySize << "maxIndex is " << maxIndex
+           << endl;
+    }
   }
-  //cout << finalMax[number] << endl;
-}
+  cout << endl << newArraySize + 1 << endl;
   return maxIndex;
-  }
+}
--- p2/main2.cpp
+++ p2/main2.cpp
@@ -1,11 +1,51 @@
 /***************************************************
   Programmer: Dillon Hendrix
-  Discription: Bonus Kattis problem if time allows
+  Discription: Kattis problem saving daylight. Convert 2 times inputted into the amount the sun is up that day. If the sun setting time is less than the sun rising time then through it out cause thats not possible. 
   Date: 10/30/22
 ***************************************************/
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  // initialize variables
+  string month;
+  string day;
+  string year;
+  
+  int firsthour, firstmin;
+  int sechour, secmin;
+  
+  // Setup a loop to read 
+  while(cin >> month && cin >> day && cin >> year){
+
+    // initialize loop variables for time added
+    int THPD = 0; // total hours per day
+    int TMPD = 0; // total mins per day
+    
+    cin >> firsthour;
+    cin.ignore();
+    cin >> firstmin;
+
+    cin >> sechour;
+    cin.ignore();
+    cin >> secmin;
+
+    // make an equation get total hours and mins per day
+    THPD = sechour - firsthour;
+    TMPD = secmin - firstmin;
+
+    // displey the month, day and year
+    cout << month << " " << day << " " << year << " ";
+
+    if(TMPD < 0){
+      TMPD += 60;
+      THPD --;
+    }
+
+  cout << THPD << " hours " << TMPD << " minutes " << endl;
+    
+  }
 }
